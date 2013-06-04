d9efa8a7c9246c1ccee5e1779d9cd4de42a12a62
Ernesto Garcia <gnapse@gmail.com>
2013-06-04 19:21:06 -0400

    Add custom plugin and theme to oh-my-zsh

---
+++ zsh/plugins/gnapse/gnapse.plugin.zsh
@@ -0,0 +1,30 @@
+c() { cd ~/code/projects/$1; }
+_c() { _files -W ~/code/projects -/; }
+compdef _c c
+
+m() { mate ~/code/projects/$1; }
+_m() { _files -W ~/code/projects -/; }
+compdef _m m
+
+v() { cd ~/code/projects/$1 && mvim && cd - > /dev/null }
+_v() { _files -W ~/code/projects -/; }
+compdef _v v
+
+#export EDITOR='mate -w'
+export EDITOR='vim'
+
+# autocorrect is more annoying than helpful
+unsetopt correct_all
+
+# a few aliases I like
+alias tlog='tail -f log/development.log'
+alias stree='open -a SourceTree'
+alias gst='git status -sb'
+alias pg-start='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
+
+# resumes partial scp downloads
+rscp() { rsync --partial --progress --rsh=ssh "$@" }
+
+# recursively download a website
+alias rwget='wget -r -k -p -np -E'
+
--- zshrc
+++ zshrc
@@ -1,11 +1,11 @@
 ZSH=$HOME/.dotfiles/oh-my-zsh
-ZSH_THEME="bira"
+ZSH_THEME="gnapse"
 
 DISABLE_AUTO_UPDATE="true"
 DISABLE_AUTO_TITLE="true"
 COMPLETION_WAITING_DOTS="true"
 
-plugins=(git osx pow bundler brew gem rails3 rvm textmate extract mvn rsync tmux tmuxinator redis-cli history git-flow cp colored-man)
+plugins=(git osx pow bundler brew gem rails3 rvm textmate extract mvn rsync tmux tmuxinator redis-cli history git-flow cp colored-man gnapse)
 
 source $ZSH/oh-my-zsh.sh
 

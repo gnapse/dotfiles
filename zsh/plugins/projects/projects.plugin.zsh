449052f3d390b2a5fe94c48b18f140d315012b4d
Ernesto Garcia <gnapse@gmail.com>
2013-08-19 20:20:44 -0400

    Prefer using vim over mvim

--- zsh/plugins/projects/projects.plugin.zsh
+++ zsh/plugins/projects/projects.plugin.zsh
@@ -6,5 +6,5 @@ compdef _projects c
 m() { mate ~/code/projects/$1; }
 compdef _projects m
 
-v() { cd ~/code/projects/$1 && mvim && cd - > /dev/null }
+v() { cd ~/code/projects/$1 && vim && cd - > /dev/null }
 compdef _projects v

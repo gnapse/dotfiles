c7e3da22d87cc9ccbff876d5144cbebebf7926a5
Ernesto Garcia <gnapse@gmail.com>
2014-03-05 10:12:34 -0300

    Remove unneeded textmate shortcut

--- zsh/plugins/projects/projects.plugin.zsh
+++ zsh/plugins/projects/projects.plugin.zsh
@@ -3,8 +3,5 @@ _projects() { _files -W ~/code/projects -/; }
 c() { cd ~/code/projects/$1; }
 compdef _projects c
 
-m() { mate ~/code/projects/$1; }
-compdef _projects m
-
 v() { cd ~/code/projects/$1 && vim && cd - > /dev/null }
 compdef _projects v
